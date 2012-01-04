8ac56a32d94bd063a819c3801c494e0288b8c596
Caleb Crane <github@simulacre.org>
2012-01-04 19:28:33 +0900

    notes about compatibility

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ cd simulacre.org
 git config receive.denyCurrentBranch ignore
 cat > .git/hooks/post-receive <<EOR
 #!/usr/bin/env bash
+cd ..
 git reset --hard
 curl http://localhost:4242/reindex/`cat ~/.ssh/id_rsa.pub  | awk '{print $2}' | ruby -r uri -ne 'print URI.escape($_.strip, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))'`/
-echo "done"
+echo -e "\ndone"
 EOR
 chmod ug+x .git/hooks/post-receive
