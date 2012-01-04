c62ce11aef4291c4d2a7e4751136a02d1a903ece
Caleb Crane <github@simulacre.org>
2012-01-04 17:58:55 +0900

    post receive hook uses reset; prod listens on 127.0.0.1 dev on 0.0.0.0

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cd simulacre.org
 git config receive.denyCurrentBranch ignore
 cat > .git/hooks/post-receive <<EOR
 #!/usr/bin/env bash
-git checkout -f
+git reset --hard
 curl http://localhost:4242/reindex/`cat ~/.ssh/id_rsa.pub  | awk '{print $2}' | ruby -r uri -ne 'print URI.escape($_.strip, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))'`/
 EOR
 chmod ug+x .git/hooks/post-receive
