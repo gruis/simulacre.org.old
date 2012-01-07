708c6e61b7c700b5f6d050b638c8bc203bb8a012
Caleb Crane <github@simulacre.org>
2012-01-07 23:11:30 +0900

    post receive hook will start thin if it isn't already

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ git config receive.denyCurrentBranch ignore
 cat > .git/hooks/post-receive <<-EOR
 #!/usr/bin/env bash
 cd ..
-echo $PWD
 env -i git reset --hard
-# @todo restart if config.ru, Gemfile, or sinatra/app.rb have been updated
+# @todo restart if config.ru or sinatra/app.rb have been updated
 # ln -f -s /var/www/simulacre.org/html/ee ../_site/ee
+[[ -e tmp/pids/thin.pid ]] || ./start
 curl http://localhost:4242/reindex/`cat ~/.ssh/id_rsa.pub  | awk '{print $2}' | ruby -r uri -ne 'print URI.escape($_.strip, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))'`/
 echo -e "\ndone"
 EOR
