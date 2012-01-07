e68604236eb0c1c50bb348d958d54e032c20cc23
Caleb Crane <github@simulacre.org>
2012-01-07 23:33:16 +0900

    increase dumb sleep by 2 seconds

--- setup.sh
+++ setup.sh
@@ -8,11 +8,12 @@ cd simulacre.org
 git config receive.denyCurrentBranch ignore
 cat > .git/hooks/post-receive <<-EOR
 #!/usr/bin/env bash
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
 cd ..
 env -i git reset --hard
 # @todo restart if config.ru or sinatra/app.rb have been updated
 # ln -f -s /var/www/simulacre.org/html/ee ../_site/ee
-[[ -e tmp/pids/thin.pid ]] || ./start
+[[ -e tmp/pids/thin.pid ]] || (./start && sleep 5)
 curl http://localhost:4242/reindex/`cat ~/.ssh/id_rsa.pub  | awk '{print $2}' | ruby -r uri -ne 'print URI.escape($_.strip, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))'`/
 echo -e "\ndone"
 EOR
