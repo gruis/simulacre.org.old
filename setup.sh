2f859c3ad0b6e48d726474b4a5db8514cae5e4b0
Caleb Crane <github@simulacre.org>
2012-01-11 11:22:57 +0900

    unset the GIT_DIR b/c bundler won't be able to install gems from git repos if it is set

--- setup.sh
+++ setup.sh
@@ -8,22 +8,28 @@ cd simulacre.org
 git config receive.denyCurrentBranch ignore
 cat > .git/hooks/post-receive <<-EOR
 #!/usr/bin/env bash
+function build_and_run {
+    [[ -z "$1" ]] || echo -e $1
+    gtd=$GIT_DIR
+    unset GIT_DIR
+    bundle install --path .bundle/gems && ./stop && sleep 2 && ./start && touch tmp/last_restart
+    GIT_DIR=$gtd
+}
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-cd ..
+cd .. 
 env -i git reset --hard
 if [ -f tmp/restart ]; then
-  if [ ! -f tmp/last_restart ]; then 
-    echo "$PWD/tmp/last_restart not found; restarting service"
-    bundle install && ./stop && sleep 2 && ./start && touch tmp/last_restart
+  if [ ! -f tmp/last_restart ]; then
+    build_and_run "$PWD/tmp/last_restart not found; restarting service"
   else
     if test tmp/restart -nt tmp/last_restart; then
-      echo "$PWD/tmp/restart has been updated; restarting service"
-      bundle install && ./stop && sleep 2 && ./start && touch tmp/last_restart
-    fi
-  fi
+      build_and_run "$PWD/tmp/restart has been updated; restarting service"
+    fi 
+  fi 
+else
+  [[ -e tmp/pids/thin.pid ]] || ./start 
 fi
-[[ -e tmp/pids/thin.pid ]] || ./start 
-# ln -f -s /var/www/simulacre.org/html/ee ../_site/ee
 sleep 5
 curl http://localhost:4242/reindex/`cat ~/.ssh/id_rsa.pub  | awk '{print $2}' | ruby -r uri -ne 'print URI.escape($_.strip, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))'`/
 echo -e "\ndone"
